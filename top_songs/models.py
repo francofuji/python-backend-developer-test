77c3a4f169a8745333cccf942e9ddcdd20fd8ff6
Francisco Pérez Ferrer <francofuji@gmail.com>
2022-10-30 16:29:36 -0300

    Creating models

--- top_songs/models.py
+++ top_songs/models.py
@@ -1,3 +1,60 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    """Model definition for Genre."""
+
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=150)
+
+    class Meta:
+        """Meta definition for Genre."""
+
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+    def __str__(self):
+        """Unicode representation of Genre."""
+        pass
+
+
+class Artist(models.Model):
+    """Model definition for Artist."""
+
+    artistName = models.CharField(max_length=100)
+    artistUrl = models.CharField(max_length=150)
+
+    class Meta:
+        """Meta definition for Artist."""
+
+        verbose_name = 'Artist'
+        verbose_name_plural = 'Artists'
+
+    def __str__(self):
+        """Unicode representation of Artist."""
+        pass
+
+
+class Track(models.Model):
+    """Model definition for Track."""
+
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=50)
+    contentAdvisoryRating = models.CharField(max_length=50, blank=True, null=True)
+    releaseDate = models.DateField()
+    artworkUrl100 = models.CharField(max_length=150)
+    url = models.CharField(max_length=150)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+
+
+    class Meta:
+        """Meta definition for Track."""
+
+        verbose_name = 'Track'
+        verbose_name_plural = 'Tracks'
+
+    def __str__(self):
+        """Unicode representation of Track."""
+        return "{} by {}".format(self.name, self.artist.name)
+    
