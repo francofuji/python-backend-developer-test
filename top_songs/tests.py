7ce4bea6d868a56f6d453ed2c4b550488fec86c2
Francisco Pérez Ferrer <francofuji@gmail.com>
2022-10-30 16:11:57 -0300

    Test case for getting a list of tracks. Failing at this point

--- top_songs/tests.py
+++ top_songs/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from top_songs.models import Track
+
+class TrackAPITestCase(APITestCase):
+
+    def test_list_tracks(self):
+        """
+        Test that we can get a list of tracks
+        """
+        response = self.client.get('/api/v0/tracks/')
+        self.assertEqual(response.status_code, 200)
