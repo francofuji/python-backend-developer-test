581a4e1bd51ee8dcadd93c64fe74c4aea6efd595
Francisco Pérez Ferrer <francofuji@gmail.com>
2022-10-31 01:41:56 -0300

    Test if database have some data. Failing at this point

--- top_songs/tests.py
+++ top_songs/tests.py
@@ -1,3 +1,5 @@
+from django.test import TestCase
+
 from rest_framework.test import APITestCase
 
 from top_songs.models import Track
@@ -10,3 +12,11 @@ class TrackAPITestCase(APITestCase):
         """
         response = self.client.get('/api/v0/tracks/')
         self.assertEqual(response.status_code, 200)
+
+class TrackDBTestCase(TestCase):
+
+    def test_db_have_data(self):
+        """
+        Test that database have some data
+        """
+        self.assertEqual(True, Track.objects.count() > 0)
