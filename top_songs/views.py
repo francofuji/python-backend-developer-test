e6c43a49f6936faba849da35877f4bd4a4e014d4
Francisco Pérez Ferrer <francofuji@gmail.com>
2022-10-31 12:33:31 -0300

    Added limit and filter options to viewset

--- top_songs/views.py
+++ top_songs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework.pagination import LimitOffsetPagination
 
 from top_songs.models import Track
 from top_songs.serializers import TrackSerializer
@@ -8,3 +10,6 @@ from top_songs.serializers import TrackSerializer
 class TrackViewSet(viewsets.ModelViewSet):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'artist__artistName']
+    pagination_class = LimitOffsetPagination
