406119b20216dff1f33fffcca70990dd4c2eb96e
Francisco Pérez Ferrer <francofuji@gmail.com>
2022-10-31 14:01:42 -0300

    Serializing

--- top_songs/serializers.py
+++ top_songs/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework.serializers import ModelSerializer
 
-from top_songs.models import Track
+from top_songs.models import Track, Artist, Genre
+
+
+class GenreSerializer(ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+class ArtistSerializer(ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'
 
 class TrackSerializer(ModelSerializer):
+    artist = ArtistSerializer()
     class Meta:
         model = Track
         fields = '__all__'
+
+    
+    def create(self, validated_data):
+        artist_data = validated_data.pop('artist')
+        artist, created = Artist.objects.get_or_create(artistUrl=artist_data['artistUrl'], defaults={
+            'artistName': artist_data['artistName']
+        })
+        genres = validated_data.pop('genres')
+        track_instance = Track.objects.create(artist=artist, **validated_data)
+        track_instance.artist = artist
+        track_instance.save()
+        for genre_data in genres:
+            track_instance.genres.add(genre_data)
+
+        return track_instance
